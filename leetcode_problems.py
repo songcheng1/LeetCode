3ccfb237787528c2be06cb6385adf886db55a343
songcheng <39392285+songcheng1@users.noreply.github.com>
2022-12-03 23:32:14 +0800

    20. 有效的括号

--- leetcode_problems.py
+++ leetcode_problems.py
@@ -71,3 +71,20 @@ class Solution:
                     break
             return s
 
+
+
+# **********************************************************************************************************
+# 20. 有效的括号
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {")": "(", "}": "{", "]": "["}
+        for char in s:
+            if char in mapping:
+                top_element = stack.pop() if stack else '#'
+                if mapping[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+        return not stack

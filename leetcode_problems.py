13a379c31cf3a8d93e8f7515f1b0897ba8120267
songcheng <39392285+songcheng1@users.noreply.github.com>
2022-12-05 09:52:45 +0800

    Update leetcode_problems.py

--- leetcode_problems.py
+++ leetcode_problems.py
@@ -1,31 +1,17 @@
 # 1.两数之和（返回对应索引）
 
-        # 答案1:
-        class Solution:
-            def twoSum(self, nums: List[int], target: int) -> List[int]:
-                lens = len(nums)
-                j=-1
-                for i in range(1,lens):
-                    temp = nums[:i]
-                    if (target - nums[i]) in temp:
-                        j = temp.index(target - nums[i])
-                        break
-                if j>=0:
-                    return [j,i]
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        lens = len(nums)
+        j=-1
+        for i in range(1,lens):
+            temp = nums[:i]
+            if (target - nums[i]) in temp:
+                j = temp.index(target - nums[i])
+                break
+        if j>=0:
+            return [j,i]
 
-        # 答案2:
-        class Solution:
-            def twoSum(self, nums: List[int], target: int) -> List[int]:
-                lens = len(nums)
-                j=-1
-                for i in range(1,lens):
-                    temp = nums[:i]
-                    if (target - nums[i]) in temp:
-                        j = temp.index(target - nums[i])
-                        break
-                if j>=0:
-                    return [j,i]
-           
 # **********************************************************************************************************
 # 9. 回文数
 

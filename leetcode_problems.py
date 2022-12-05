84c528a815b3219973d7cdc3fe210934adc1ea8f
songcheng <39392285+songcheng1@users.noreply.github.com>
2022-12-05 09:42:15 +0800

    70. 爬楼梯

--- leetcode_problems.py
+++ leetcode_problems.py
@@ -159,3 +159,12 @@ class Solution:
             fast += 1
         return slow
 
+# **********************************************************************************************************
+# 70. 爬楼梯
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        res = [0] * (n+1)
+        res[0] = res[1] =1 
+        for i in range(2,n+1):
+            res[i] = res[i-1] + res[i-2]
+        return res[-1]  
